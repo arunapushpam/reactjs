2eea2d50102f91c8a7aa66dd32f3d2aac8a48afa
Anitha <arunaani2000@gmail.com>
2023-03-13 22:09:06 +0530

    add folder

---
+++ file1.js
@@ -0,0 +1,9 @@
+// import get,{name}from",/file2.js";
+// import {main}from",/file2.js";
+// console.log(name)
+// console.log(main(),get())
+
+export var a=10;
+console.log(a);
+export var b=5;
+console.log(b);
