0757d2c00ba1df7f320da7d4eaa72e559af328ec
Anitha <arunaani2000@gmail.com>
2023-03-14 12:39:20 +0530

    add fodler

---
+++ file2.js
@@ -0,0 +1,15 @@
+// export const name="aruna";
+// export const main=()=>{
+//     return setTimeout(function(){
+//       console.log("Hii")
+//     },3000)
+// }
+// export default()=>{
+//     console.log("hii all")
+// }
+
+import {a} from "./file1.js"
+
+console.log(a);
+import {b} from "./file1.js"
+console.log(b);
